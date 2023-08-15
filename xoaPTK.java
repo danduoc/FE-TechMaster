8101c7b0771027bd6b4686926b39c8303f0eacde
Hoàng Tuấn <140074168+danduoc@users.noreply.github.com>
2023-08-15 22:41:44 +0900

    Add files via upload

---
+++ xoaPTK.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+import javax.swing.ButtonGroup;
+
+import java.lang.Math;
+
+public class xoaPTK {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("N=");
+		int n = sc.nextInt();
+		int[] a;
+		a = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			System.out.print("a [" +i+ "] = ");
+			a[i] = sc.nextInt();
+		}
+		System.out.print("Nhập số K cần xoá: ");
+        int k = sc.nextInt();
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+        	if (a[i] == k) {
+        		j=i;
+        		for(i=j ; i < n-1 ; i++){
+        			a[i] = a[i+1];
+        		}
+        		n--;
+        	}
+     
+        }
+        System.out.println("mảng sau khi xóa "+k+" : ");
+        for (int i = 0; i < n; i++) {
+			System.out.println("a [" +i+ "] = " +a[i]);
+        }
+	}
+}
