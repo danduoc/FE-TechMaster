2adc204634ccb498eb2ebfa7cc5dc870de648c98
Hoàng Tuấn <140074168+danduoc@users.noreply.github.com>
2023-08-24 22:53:08 +0900

    Update DemChuoiCon.java

--- DemChuoiCon.java
+++ DemChuoiCon.java
@@ -7,15 +7,15 @@ public class DemChuoiCon {
 		String str1 = sc.nextLine();
 		System.out.print("Nhập chuỗi 2: ");
 		String str2 = sc.nextLine();
-		int k = 0;
+		int dem = 0;
 		for (int i = 0; i < str1.length(); i++) {
 			if (str1.charAt(i) == str2.charAt(0)) {
 				if (str1.substring(i, i + str2.length()).equals(str2)) {
-					k++;
+					dem++;
 				}
 			}
 
 		}
-		System.out.print("Chuỗi " + str2 + " xuất hiện " + k + " lần trong chuỗi " + str1);
+		System.out.print("Chuỗi " + str2 + " xuất hiện " + dem + " lần trong chuỗi " + str1);
 	}
 }
