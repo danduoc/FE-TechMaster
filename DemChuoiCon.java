4f48b2015667a857ffffdf414a57cd524fd43fb8
Hoàng Tuấn <140074168+danduoc@users.noreply.github.com>
2023-08-24 22:46:02 +0900

    Update DemChuoiCon.java

--- DemChuoiCon.java
+++ DemChuoiCon.java
@@ -10,7 +10,7 @@ public class DemChuoiCon {
 		int k = 0;
 		for (int i = 0; i < str1.length()-str2.length(); i++) {
 			if (str1.charAt(i) == str2.charAt(0)) {
-				if (str1.substring(i, i+str2.length()).equals(str2)) {
+				if (str1.substring(i, i + str2.length()).equals(str2)) {
 					k++;
 				}
 			}
