c0a804a2e2a8029e624c73baf9ab58f0fa733275
Hoàng Tuấn <140074168+danduoc@users.noreply.github.com>
2023-09-07 22:01:05 +0900

    Update main.java

--- main.java
+++ main.java
@@ -2,15 +2,19 @@ import java.util.ArrayList;
 
 public class main {
 	public static void main(String[] args) {
-		
+
 		ArrayList<PT> listPT = new ArrayList<>();
 		listPT.add(new PT("Tuan", 36, "080-2040-5465", "men", 112, 1200));
 		listPT.add(new PT("Truc Anh", 8, "080-2040-5465", "women", 120, 1250));
-		listPT.add(new PT("Nhat",28,"1111","men",110,1200));
+		listPT.add(new PT("Nhat", 28, "1111", "men", 110, 1200));
 		ArrayList<FT> listFT = new ArrayList<>();
 		listFT.add(new FT("Tu Anh", 35, "070-3795-0002", "women", 22, 10000));
-		listFT.add(new FT("Huong",29,"1111","women",21,10000));
-		listFT.add(new FT("Cuong",32,"0000","men",25,11000));
+		listFT.add(new FT("Huong", 29, "1111", "women", 21, 10000));
+		listFT.add(new FT("Cuong", 32, "0000", "men", 25, 11000));
+		ArrayList<Person> persons = new ArrayList<>();
+		persons.addAll(listFT);
+		persons.addAll(listPT);
+
 		ArrayList<PT> maxSalaryPT = new ArrayList<PT>();
 		maxSalaryPT = PT.findMaxSalary(listPT);
 		for (Person p : maxSalaryPT) {
@@ -19,6 +23,11 @@ public class main {
 		ArrayList<FT> maxSalaryFT = new ArrayList<FT>();
 		maxSalaryFT = FT.findMaxSalary(listFT);
 		for (Person p : maxSalaryFT) {
+			p.display();
+		}
+		ArrayList<Person> findPersons = new ArrayList<>();
+		findPersons = Person.findPersonByName(persons, "Tu Anh");
+		for (Person p : findPersons) {
 			p.display();
 		}
 	}
